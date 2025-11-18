b67f368186f836fdbf0b6de52279b1e86ba01239
Sasha <o.volovikk@gmail.com>
2025-11-19 00:17:15 +0200

    initialized window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 int main()
 {
     std::cout << "Cubify!" << std::endl;
+
+    glewExperimental = true;
+    if(!glfwInit())
+    {
+        std::cout << "glfw init error.\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for MacOS
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1024, 768, "Cubify", NULL, NULL);
+    if(window == nullptr)
+    {
+        std::cout << "window init error.\n";
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "glew init error.\n";
+        return -1;
+    }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glClearColor(0.f, 0.f, 0.4f, 0.f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    do {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER);
+        // drawing here
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while(glfwWindowShouldClose(window) == 0);
+
+    glfwTerminate();
     return 0;
 }
