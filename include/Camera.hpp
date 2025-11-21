c2b39ee2cbfede9ace5ddf1da6453e0781ed9dce
Sasha <o.volovikk@gmail.com>
2025-11-21 15:52:01 +0200

    Added setters for Uniform Values

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -1,18 +1,20 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <GL/glew.h>
 #include <glm/glm.hpp>
 using glm::mat4;
 
 class Camera
 {
 public:
-    Camera(mat4 projection_mat, mat4 model_mat, mat4 view_mat);
+    Camera(mat4 _projection_mat, mat4 _model_mat, mat4 _view_mat);
 
 private:
-    mat4 ProjectionMat;
-    mat4 ModelMat;
-    mat4 ViewMat;
+    GLuint matrix_ID;
+    mat4 projection_mat;
+    mat4 model_mat;
+    mat4 view_mat;
     mat4 MVP;
 };
 
