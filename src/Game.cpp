f5610e772c072a4e833646e1fb62f0f056cd1f74
Sasha <o.volovikk@gmail.com>
2025-12-03 17:21:25 +0200

    Changed to oGL 4.3

--- src/Game.cpp
+++ src/Game.cpp
@@ -28,7 +28,7 @@ void Game::init(const std::string& title)
     }
 
     glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for MacOS
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
